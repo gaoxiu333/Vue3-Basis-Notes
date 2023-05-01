ca0549b93dafa6669a7cb8b1385e42ad2d670e11
xiu <gao.yh1991@gmail.com>
2023-05-01 11:19:32 +0800

    更新README

--- demo/vue3-learning/src/App.vue
+++ demo/vue3-learning/src/App.vue
@@ -1,47 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Layout>
+    <template #header>
+      <h1>test</h1>
+    </template>
+    <template #content>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    </template>
+  </Layout>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script setup lang="ts">
+import Layout from './components/Layout/index.vue'
+import Base from './learnApi/Base.vue';
+import CompositionAPI from './learnApi/CompositionAPI.vue';
+const lists = [
+  {
+    name: 'base',
+    component: Base
+  },
+  {
+    name: 'base',
+    component: CompositionAPI
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+]
+</script>
+<style scoped></style>
---
+++ demo/vue3-learning/src/components/Layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <slot name="header"></slot>
+    </header>
+    <section class="content">
+        <slot name="content"></slot>
+    </section>
+</template>
+<style scoped>
+.header {
+    font-size: 24px;
+}
+
+.content {
+    border: 1px solid red;
+}
+</style>
